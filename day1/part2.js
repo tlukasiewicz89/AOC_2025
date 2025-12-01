bcdb207b3f0f60c19b185530ca705b0bdcbb0235
Thomas Lukasiewicz <tom@MacBookPro.lan>
2025-12-01 17:53:08 -0600

    day 1 complete

---
+++ day1/part2.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+const text = fs.readFileSync("data.txt", "utf8").trim();
+const lines = text.split("\n");
+
+let current = 50;
+
+let answer = 0;
+
+const rotate = (d) => {
+    const direction = d[0];
+    let distance = Number(d.slice(1));
+
+    if (direction === "R"){
+        while (distance){
+            if (current === 99){
+                current = 0;
+            } else {
+                current++;
+            }
+            distance--;
+            if (current === 0) answer++;
+        }
+    }
+    if (direction === 'L'){
+        while (distance){
+            if (current === 0){
+                current = 99;
+            } else {
+                current--;
+            }
+            distance--;
+            if (current === 0) answer++;
+        }
+    }
+}
+
+lines.forEach(line=>rotate(line));
+
+console.log('ANSWER:', answer)
